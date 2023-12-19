65235180f93070df41b7fae6a231f2967d06f368
Tegar Ferdigantara <tegarferdigantara007@gmail.com>
2023-12-20 01:11:37 +0700

    alert fix v1.1

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -149,7 +149,7 @@
                 integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
             </script>
 
-            <script src="alert-custom.js"></script>
+            <script src="js/alert-custom.js"></script>
             <!-- plugins:js -->
             <script src="vendors/js/vendor.bundle.base.js"></script>
             <!-- endinject -->
